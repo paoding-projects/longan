ce4d1d470c4fc75a6c73ea8a1aff18e9dd7ab4e1
enhydra <enhydra@gmail.com>
2026-07-05 16:21:23 +0800

    Refactor ListMethodInterceptor and remove unused fields and imports

--- src/main/java/dev/paoding/longan/data/jpa/ListMethodInterceptor.java
+++ src/main/java/dev/paoding/longan/data/jpa/ListMethodInterceptor.java
@@ -3,8 +3,6 @@ package dev.paoding.longan.data.jpa;
 import com.google.common.collect.ImmutableMap;
 import dev.paoding.longan.util.EntityUtils;
 import dev.paoding.longan.util.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
 
@@ -13,15 +11,13 @@ import java.util.List;
 import java.util.Map;
 
 public class ListMethodInterceptor<T> implements MethodInterceptor {
-    private final Logger logger = LoggerFactory.getLogger(ListMethodInterceptor.class);
     private final JdbcSession jdbcSession;
-    private boolean isLoaded;
-    private Class<?> many;
+    private final Class<?> many;
     private List<T> list;
-    private Object bean;
-    private Role role;
+    private final Object bean;
+    private final Role role;
     private String joinFile;
-    private MetaTable<T> metaTable;
+    private final MetaTable<T> metaTable;
 
     public ListMethodInterceptor(JdbcSession jdbcSession, Class<?> one, Class<T> many, Role role, String joinFile, Object bean) {
         this.metaTable = MetaTableFactory.get(many);
@@ -37,7 +33,7 @@ public class ListMethodInterceptor<T> implements MethodInterceptor {
 
     @Override
     public Object intercept(Object object, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
-        if (!isLoaded) {
+        if (list == null) {
             load();
         }
         return method.invoke(list, args);
@@ -47,18 +43,15 @@ public class ListMethodInterceptor<T> implements MethodInterceptor {
     private void load() {
         if (role == null) {
             Map<String, Object> map = ImmutableMap.of(joinFile, bean);
-//            MethodParser methodParser = MethodParser.of("select", many, StringUtils.upperFirst(joinFile), map, role);
             MethodParser methodParser = MethodParser.of(jdbcSession.getDatabaseType(),"select", many, StringUtils.upperFirst(joinFile), map);
             String sql = methodParser.getSql();
             this.list = jdbcSession.query(sql, methodParser.getParamMap(), metaTable.getRowMapper());
         } else {
             Map<String, Object> map = ImmutableMap.of(role.getName(), bean);
-//            MethodParser methodParser = MethodParser.of("select", many, StringUtils.upperFirst(role.getName()), map, role);
             MethodParser methodParser = MethodParser.of(jdbcSession.getDatabaseType(),"select", many, StringUtils.upperFirst(role.getName()), map);
             String sql = methodParser.getSql();
             this.list = jdbcSession.query(sql, methodParser.getParamMap(), metaTable.getRowMapper());
         }
-        isLoaded = true;
         EntityUtils.wrap(metaTable, list);
     }
 }
